dfcdf2095bbe9f3cc243f82ceffbba3d8dbe80a9
vitbla <vitbla07@gmail.com>
2026-03-26 14:40:46 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const PisunText=document.getElementById("pisun-text");
+const pidorbtn=document.getElementById("bebra")
+pidorbtn.addEventListener('click', (e)=>{
+    PisunText.textContent="pisun2";
+});
